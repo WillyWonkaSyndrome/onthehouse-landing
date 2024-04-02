0ecd5a1683f5b7f21ea31be503faee6c811344d5
SeyoungCho <wnl383@naver.com>
2024-04-02 17:27:13 +0900

    feat: add contact submit feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
+  // output: "server",
   integrations: [
     tailwind(),
     icon({
